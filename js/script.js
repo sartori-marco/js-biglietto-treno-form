1a324c9b74a40dac18d980a984fec97c2c5d68b3
marco sartori <marcosartori98@yahoo.com>
2020-12-15 15:48:17 +0100

    start js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// VARIABILI
+var nomeInput = document.getElementById('nome').value;
+console.log(nomeInput);
 
+var distanzaInput  = document.getElementById('chilometri').value;
+console.log(distanzaInput);
+
+var etaInput = document.getElementById('eta').value;
+console.log(etaInput);
